0dcb3e1ed704704a32952d435466092c2ac383b2
sai <52447619+saikumar246@users.noreply.github.com>
2023-03-10 12:38:36 +0530

    Update githubtest.py
    committing test

--- notebooks/Users/saikumar.divvela@databricks.com/githubtest.py
+++ notebooks/Users/saikumar.divvela@databricks.com/githubtest.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-#this is a python programks
+#this is a python programs -sai
 print("Hello databricks")
 #below line updated in the github
 print("This line is added in the github")
